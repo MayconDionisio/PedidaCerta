986f58a20777b943077d48e42942f2371516bbf0
Maycon Dionisio <maycon.dionisio@gmail.com>
2017-02-22 23:45:41 -0300

    first commit

---
+++ index.android.js
@@ -0,0 +1,48 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  MapView
+} from 'react-native';
+
+export default class PedidaCerta extends Component {
+  render() {
+    return (
+    <View>
+      <MapView
+        style={{height: 200, margin: 40, width:300}}
+        showsUserLocation={true}
+      />
+    </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('PedidaCerta', () => PedidaCerta);
